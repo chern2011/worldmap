99f1508769ae71addfcd516fc8dea8904955fa70
Che Her <chern2011@gmail.com>
2021-07-21 19:39:26 -0400

    React Native Components Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import { PLACES } from '../shared/places';
+import PlaceInfo from './PlaceInfoComponent';
+import { View } from 'react-native';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            places: PLACES
+            places: PLACES,
+            selectedPlace: null
         };
     }
 
+    onPlaceSelect(placeId) {
+        this.setState({selectedPlace: placeId});
+    }
+
     render() {
-        return <Directory places={this.state.places} />;
+        return (
+            <View style={{flex: 1}}>
+                <Directory
+                    places={this.state.places}
+                    onPress={placeId => this.onPlaceSelect(placeId)}
+                />
+                <PlaceInfo
+                    place={this.state.places.filter(
+                        place => place.id === this.state.selectedPlace)[0]}
+                />
+            </View>
+        )
     }
 }
 
