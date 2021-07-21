c22d671671e9abdf78207020036214f3944dc2c8
Che Her <chern2011@gmail.com>
2021-07-21 19:27:31 -0400

    React Native Components Part 1

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,42 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
-import { PLACES } from '../shared/places';
 
-class Directory extends Component {
+function Directory(props) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            places: PLACES
-        };
-    }
-
-    static navigationOptions = {
-        title: 'Directory'
-    }
-
-    render() {
-        const { navigate } = this.props.navigation;
-        const renderDirectoryItem = ({item}) => {
-            return (
-                <ListItem
-                    title={item.name}
-                    subtitle={item.description}
-                    onPress={() => navigate('PlaceInfo', { placeId: item.id })}
-                    leftAvatar={{ source: require('./image/WorldMap.jpg')}}
-                />
-            );
-        };
-
+    const renderDirectoryItem = ({item}) => {
         return (
-            <FlatList
-                data={this.state.places}
-                renderItem={renderDirectoryItem}
-                keyExtractor={item => item.id.toString()}
+            <ListItem
+                title={item.name}
+                subtitle={item.description}
+                leftAvatar={{ source: require('./image/WorldMap.jpg')}}
             />
         );
-    }
+    };
+
+    return (
+        <FlatList
+            data={props.places}
+            renderItem={renderDirectoryItem}
+            keyExtractor={item => item.id.toString()}
+        />
+    );
 }
 
 export default Directory;
