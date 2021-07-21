326e8f54b9f10eec7d404cf53ccd06f81305aea0
Che Her <chern2011@gmail.com>
2021-07-21 19:49:22 -0400

    Exercise: Navigation Part 1

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,6 +1,52 @@
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { PLACES } from '../shared/places';
+
+class Directory extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            places: PLACES
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Directory'
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+        const renderDirectoryItem = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    onPress={() => navigate('PlaceInfo', { placeId: item.id })}
+                    leftAvatar={{ source: require('./image/WorldMap.jpg')}}
+                />
+            );
+        };
+
+        return (
+            <FlatList
+                data={this.state.places}
+                renderItem={renderDirectoryItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        );
+    }
+}
+
+export default Directory;
+
+
+/*
 import React from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { PLACES } from '../shared/places';
 
 function Directory(props) {
 
@@ -10,7 +56,7 @@ function Directory(props) {
                 title={item.name}
                 subtitle={item.description}
                 onPress={() => props.onPress(item.id)}
-                leftAvatar={{ source: require('./image/WorldMap.jpg')}}
+                leftAvatar={{ source: require('./Desktop/React Native Profile Project/React Native Assignment/image/WorldMap.jpg')}}
             />
         );
     };
@@ -25,3 +71,4 @@ function Directory(props) {
 }
 
 export default Directory;
+*/
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,9 +2,49 @@ import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import { PLACES } from '../shared/places';
 import PlaceInfo from './PlaceInfoComponent';
-import { View } from 'react-native';
+import Constants from 'expo-constants';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        PlaceInfo: { screen: PlaceInfo }
+    }, 
+    {
+        initialRouteName: 'Directory',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const AppNavigator = createAppContainer(DirectoryNavigator);
 
 class Main extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+            }}>
+                <AppNavigator />
+            </View>
+        );
+    }
+}
+
+export default Main;
+
+/*class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -24,13 +64,12 @@ class Main extends Component {
                     places={this.state.places}
                     onPress={placeId => this.onPlaceSelect(placeId)}
                 />
-                <PlaceInfo
+                <Placeinfo
                     place={this.state.places.filter(
                         place => place.id === this.state.selectedPlace)[0]}
                 />
             </View>
-        )
     }
 }
 
-export default Main;
+export default Main; */
