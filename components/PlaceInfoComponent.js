daaa3e9db4d47f637e60674e35f56f8130c5e381
Che Her <chern2011@gmail.com>
2021-07-26 09:35:16 -0400

    Icons, Favorites, and Comments

--- components/PlaceInfoComponent.js
+++ components/PlaceInfoComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, View, ScrollView, FlatList } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
 import { PLACES } from '../shared/places';
+import { COMMENTS } from '../shared/comments';
 
-function RenderPlace({place}) {
+function RenderPlace(props) {
+    const {place} = props;
     if (place) {
         return (
             <Card 
@@ -13,10 +15,43 @@ function RenderPlace({place}) {
                 <Text style={{margin: 10}}>
                     {place.description}
                 </Text>
+                <Icon
+                    name={props.favorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() => props.favorite ? 
+                        console.log('Already set as a favorite') : props.markFavorite()}
+                />
             </Card>
         );
     }
     return <View />;
+}
+
+
+function RenderComments({comments}) {
+
+    const renderCommentItem = ({item}) => {
+        return (
+            <View style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.text}</Text>
+                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+                <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
+            </View>
+        );
+    };
+
+    return (
+        <Card title='Comments'>
+            <FlatList
+                data={comments}
+                renderItem={renderCommentItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        </Card>
+    );
 }
 
 class PlaceInfo extends Component {
@@ -24,8 +59,14 @@ class PlaceInfo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            places: PLACES
+            places: PLACES,
+            comments: COMMENTS,
+            favorite: false
         };
+    }
+
+    markFavorite() {
+        this.setState({favorite: true});
     }
 
     static navigationOptions = {
@@ -35,7 +76,16 @@ class PlaceInfo extends Component {
     render() {
         const placeId = this.props.navigation.getParam('placeId');
         const place = this.state.places.filter(place => place.id === placeId)[0];
-        return <RenderPlace place={place} />;
+        const comments = this.state.comments.filter(comment => comment.placeId === placeId);
+        return (
+            <ScrollView>
+                <RenderPlace place={place}
+                    favorite={this.state.favorite}
+                    markFavorite={() => this.markFavorite()}
+                />
+                <RenderComments comments={comments} />
+            </ScrollView>
+        );
     }
 }
 
