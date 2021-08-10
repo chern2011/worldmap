f8157c36406685f111b5aa1c85c1ffe30bb42023
Che Her <chern2011@gmail.com>
2021-08-10 14:21:25 -0400

    Animatable

--- components/PlaceInfoComponent.js
+++ components/PlaceInfoComponent.js
@@ -6,6 +6,7 @@ import { COMMENTS } from '../shared/comments';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -20,26 +21,30 @@ const mapDispatchToProps = {
 };
 
 function RenderPlace(props) {
+
     const {place} = props;
+
     if (place) {
         return (
-            <Card 
-                featuredTitle={place.name}
-                image={{uri: baseUrl + place.image}}
-            >
-                <Text style={{margin: 10}}>
-                    {place.description}
-                </Text>
-                <Icon
-                    name={props.favorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
-                    raised
-                    reverse
-                    onPress={() => props.favorite ? 
-                        console.log('Already set as a favorite') : props.markFavorite()}
-                />
-            </Card>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card
+                    featuredTitle={place.name}
+                    image={{uri: baseUrl + place.image}}
+                    >
+                    <Text style={{margin: 10}}>
+                        {place.description}
+                    </Text>
+                    <Icon
+                        name={props.favorite ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        raised
+                        reverse
+                        onPress={() => props.favorite ? 
+                            console.log('Already set as a favorite') : props.markFavorite()}
+                    />
+                </Card>
+            </Animatable.View>
         );
     }
     return <View />;
@@ -59,13 +64,15 @@ function RenderComments({comments}) {
     };
 
     return (
-        <Card title='Comments'>
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </Card>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title='Comments'>
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Card>
+        </Animatable.View>
     );
 }
 
